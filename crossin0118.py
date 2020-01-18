5a9de504c8f4c8e20d3a0ce596537e545d9354ec
z-c1988 <59910280+z-c1988@users.noreply.github.com>
2020-01-18 15:28:48 +0900

    Create crossin0118.py

---
+++ crossin0118.py
@@ -0,0 +1,21 @@
+#练习一
+guess = input("请猜一个数字:")
+print("请猜一个数字：" + guess)
+print("你输入的数字是: " + guess)
+
+#练习二
+money = 20
+result = money * 2 + 50
+print(result)
+
+#练习三
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
