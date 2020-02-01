9cb8c9b92c611022300f7f2e9e4c982c94c44943
z-c1988 <59910280+z-c1988@users.noreply.github.com>
2020-02-01 15:28:57 +0900

    Update crossin0201.py

--- crossin0201.py
+++ crossin0201.py
@@ -1,8 +1,34 @@
 #1. if条件判断
 money = 88
-
-# 当 money 的值大于 100 时，输出 "rich"
-# 否则，输出 "poor"
-
-if money>100
+if money>100:
     print("rich")
+else:
+    print("poor")
+
+#2. 逻辑判断
+(a) == True
+(b) == False
+(c) 
+请输入内容1
+True
+请输入内容
+False
+(d) 
+False
+True
+False
+True
+False
+True
+False
+
+#3. BMI 计算器
+height = float(input("height:"))
+weight = float(input("weight:"))
+BMI = weight/(height*height)      
+if BMI < 18.5:
+    print("体重偏轻")
+elif 18.5 <= BMI < 24:
+    print("体重正常")
+elif BMI >= 24:
+    print("体重偏重")
