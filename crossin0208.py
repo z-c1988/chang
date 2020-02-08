7a6d0cf7a2470355730c90d6d3b1eedca7221863
z-c1988 <59910280+z-c1988@users.noreply.github.com>
2020-02-08 17:51:47 +0900

    Create crossin0208.py

---
+++ crossin0208.py
@@ -0,0 +1,22 @@
+#1. 猜数字小游戏
+num = 0
+print('Guess what I think?')
+while num != 10:
+    print('please guess a number')
+    num = int(input())
+print('Bingo!')
+
+#2. while, continue
+count = 0
+while count<11
+    count += 1
+    if count == 4 or count == 5
+        continue
+    print(count)
+    
+#3. 使用 while 计算100以内所有3的倍数的和（即：计算3、6、9 ... 96、99 的和）
+sum = 0
+for num in range(101):
+    while num % 3 == 0:
+        sum += num
+print(sum)
